c9e3f0bd53f9d9c8195be84603d5c8e9cc3e40f3
deathKiller10 <priyanshuwork.10@gmail.com>
2024-12-28 20:49:21 +0530

    incorporated react router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,22 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from 'react-router'
+import Home from './components/Home/Home.jsx'
+import { Layout } from '../Layout.jsx'
+import { Products } from './components/Products/Products.jsx'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path="" element={<Home />}></Route>
+      <Route path="products" element={<Products />}></Route>
+    </Route>
+  )
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
