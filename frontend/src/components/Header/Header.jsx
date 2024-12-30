030a81932f02fd6ee4c1edbe055e2f231bd1c12b
deathKiller10 <priyanshuwork.10@gmail.com>
2024-12-30 16:00:45 +0530

    minor tweaks

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -8,11 +8,11 @@ export const Header = ({toggleMenu}) => {
   return (
     <div className="h-[75px] bg-gray-900 text-white shadow-md sticky top-0">
       <div className="flex justify-between mx-4 justify-center items-center h-full">
-        <RxHamburgerMenu size={24} className="hover:scale-[1.15] hover:cursor-pointer" onClick={toggleMenu}/>
-        <PiSigmaBold size={24} className="hover:cursor-pointer"/>
+        <RxHamburgerMenu size={24} className="transition-all duration-300 hover:scale-125 hover:cursor-pointer" onClick={toggleMenu}/>
+        <PiSigmaBold size={24} className="transition-all duration-300 hover:scale-125 hover:cursor-pointer"/>
         <ul className="flex gap-4">
-            <li className="hover:scale-[1.15] hover:cursor-pointer"><IoSearch size={24}/></li>
-            <li className="hover:scale-[1.15] hover:cursor-pointer"><FaCartShopping size={24}/></li>
+            <li className="transition-all duration-300 hover:scale-125 hover:cursor-pointer"><IoSearch size={24}/></li>
+            <li className="transition-all duration-300 hover:scale-125 hover:cursor-pointer"><FaCartShopping size={24}/></li>
         </ul>
       </div>
     </div>
--- frontend/src/components/Menu/Menu.jsx
+++ frontend/src/components/Menu/Menu.jsx
@@ -1,32 +1,30 @@
 import React from 'react'
+import { NavLink } from 'react-router'
 
 export const Menu = ({isMenuOpen, toggleMenu}) => {
-  return (
-    <div>
-        {/* Overlay */}
-        {isMenuOpen && (
+    return (
+        <div>
+            {/* Overlay */}
+            {isMenuOpen && (
+                <div
+                className="fixed inset-0 bg-black bg-opacity-70"
+                onClick={toggleMenu}
+                ></div>
+            )}
+            {/* Menu Panel */}
             <div
-            className="fixed inset-0 bg-black bg-opacity-70"
-            onClick={toggleMenu}
-            ></div>
-        )}
-
-        {/* Menu Panel */}
-        <div
-            className={`fixed top-0 left-0 w-2/3 h-full bg-gray-800 text-white shadow-lg transform ${
-            isMenuOpen ? "translate-x-0" : "-translate-x-full"
-            } transition-transform duration-300`}
-        >
-            <button className="absolute top-4 right-4 text-xl" onClick={toggleMenu}>
-            ✖
-            </button>
-            <nav className="mt-16 space-y-2">
-            <a href="#" className="block px-4 py-2 hover:bg-gray-100 hover:text-black">Home</a>
-            <a href="#" className="block px-4 py-2 hover:bg-gray-100 hover:text-black">Products</a>
-            <a href="#" className="block px-4 py-2 hover:bg-gray-100 hover:text-black">Cart</a>
-            <a href="#" className="block px-4 py-2 hover:bg-gray-100 hover:text-black">Contact Us</a>
-            </nav>
+                className={`fixed top-0 left-0 w-2/3 h-full bg-gray-800 text-white shadow-lg transform ${
+                isMenuOpen ? "translate-x-0" : "-translate-x-full"
+                } transition-transform duration-300`}
+            >
+                <button className="absolute top-4 right-4 text-xl" onClick={toggleMenu}>✖</button>
+                <nav className="mt-16 space-y-2">
+                    <NavLink to="/" className="block px-4 py-2 hover:bg-gray-100 hover:text-black text-xl" onCLick={toggleMenu}>Home</NavLink>
+                    <NavLink to="/products" className="block px-4 py-2 hover:bg-gray-100 hover:text-black text-xl" onCLick={toggleMenu}>Products</NavLink>
+                    <NavLink to="/" className="block px-4 py-2 hover:bg-gray-100 hover:text-black text-xl">Cart</NavLink>
+                    <NavLink to="/" className="block px-4 py-2 hover:bg-gray-100 hover:text-black text-xl">Contact Us</NavLink>
+                </nav>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
